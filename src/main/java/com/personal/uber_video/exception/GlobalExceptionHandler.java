2e6c6a01fb96025939dcac5a19ac4e071873366a
Satya Prakash <pandsath@amazon.com>
2025-11-30 20:30:18 +0530

    Exception added

--- src/main/java/com/personal/uber_video/exception/GlobalExceptionHandler.java
+++ src/main/java/com/personal/uber_video/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.personal.uber_video.exception;
 import com.personal.uber_video.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +16,13 @@ public class GlobalExceptionHandler {
         String message = e.getMessage();
         ApiResponse apiResponse = new ApiResponse(message);
         return new ResponseEntity<>(apiResponse, e.getStatusCode());
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public  ResponseEntity<?> handleUsernameNotFoundException(UsernameNotFoundException e) {
+        String message = e.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
 }
