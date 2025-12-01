cb058d645070c2000d548469e282e6a95708ec06
Satya Prakash <pandsath@amazon.com>
2025-12-01 21:18:23 +0530

    Location add

---
+++ src/main/java/com/personal/uber_video/model/Location.java
@@ -0,0 +1,15 @@
+package com.personal.uber_video.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location {
+    private Double latitude;
+    private Double longitude;
+}
